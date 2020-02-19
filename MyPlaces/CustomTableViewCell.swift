d123b95ddaf915eed515798e1c26bd2375a3a22a
Roman <elfiroma@gmail.com>
2020-02-19 14:19:02 +0300

    Custom Table View Cell

---
+++ MyPlaces/CustomTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  CustomTableViewCell.swift
+//  MyPlaces
+//
+//  Created by Рома on 19.02.2020.
+//  Copyright © 2020 Рома. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var lacationLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    
+}
