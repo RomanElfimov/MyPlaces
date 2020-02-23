91c6dcb880abdb9181c72994b9d0821af9fa4ea8
Роман <elfiroma@gmail.com>
2020-02-23 16:54:27 +0300

    make sorting add search bar

--- MyPlaces/MainViewController.swift
+++ MyPlaces/MainViewController.swift
@@ -10,19 +10,56 @@ import UIKit
 import RealmSwift
 
 class MainViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
-
+    
+    private let searchController = UISearchController(searchResultsController: nil)
+    
+    //коллекция Results (default для Realm) с типом Place
+    private var places: Results<Place>!
+    //массив для отфильтрованных записей
+    private var filteredPlaces: Results<Place>!
+    //сортировка по возрастанию
+    private var ascendingSorting = true
+    //строка поиска путая или нет
+    private var searchBarIsEmpty: Bool {
+        guard let text = searchController.searchBar.text else { return false }
+        return text.isEmpty
+    }
+    private var isFiltering: Bool {
+        return searchController.isActive && !searchBarIsEmpty
+    }
+    
     @IBOutlet weak var tableView: UITableView!
-    var places: Results<Place>!
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    @IBOutlet weak var reversedSortingButton: UIBarButtonItem!
+   
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         places = realm.objects(Place.self)
+        
+        //Setup the search controller
+        searchController.searchResultsUpdater = self
+        //позволяем контроллеру взаимодействовать с отображаемым контентом
+        searchController.obscuresBackgroundDuringPresentation = false
+        //название для строки поиска
+        searchController.searchBar.placeholder = "Search"
+        //строка поиска интегрирована в navigation bar
+        navigationItem.searchController = searchController
+        //отпустить строку поиска при переходе на другой экран
+        definesPresentationContext = true
+        
+        navigationItem.hidesSearchBarWhenScrolling = true
     }
+    
+    
 
     // MARK: - Table view data source
 
      func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if isFiltering {
+            return filteredPlaces.count
+        }
         return places.isEmpty ? 0 : places.count
     }
 
@@ -30,7 +67,14 @@ class MainViewController: UIViewController, UITableViewDataSource, UITableViewDe
      func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! CustomTableViewCell
 
-        let place = places[indexPath.row]
+        var place = Place()
+        
+        if isFiltering {
+            place = filteredPlaces[indexPath.row]
+        } else {
+            place  = places[indexPath.row]
+            
+        }
 
         cell.nameLabel.text = place.name
         cell.lacationLabel.text = place.location
@@ -58,10 +102,16 @@ class MainViewController: UIViewController, UITableViewDataSource, UITableViewDe
 
     
     // MARK: - Navigation
+    //передачва данных на detail view controller
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "showDetail" {
             guard let indexPath = tableView.indexPathForSelectedRow else { return }
-            let place = places[indexPath.row]
+            let place: Place
+            if isFiltering {
+                place = filteredPlaces[indexPath.row]
+            } else {
+                place = places[indexPath.row]
+            }
             let newPlaceVC = segue.destination as! NewPlaceViewController
             newPlaceVC.currentPlace = place
         }
@@ -77,5 +127,55 @@ class MainViewController: UIViewController, UITableViewDataSource, UITableViewDe
         
         tableView.reloadData()
     }
+    
+    @IBAction func sotrSelection(_ sender: UISegmentedControl) {
+        sorting()
+    }
+    
+    @IBAction func reversedSorting(_ sender: Any) {
+        
+        //меняем значение на противополжное - toggle()
+        ascendingSorting.toggle()
+        
+        //меняем картинку на кнопке
+        if ascendingSorting {
+            reversedSortingButton.image = #imageLiteral(resourceName: "AZ")
+        } else {
+            reversedSortingButton.image = #imageLiteral(resourceName: "ZA")
+        }
+        
+        sorting()
+    }
+    
+    //метод для сортировки
+    private func sorting() {
+        
+        if segmentedControl.selectedSegmentIndex == 0 {
+            places = places.sorted(byKeyPath: "date", ascending: ascendingSorting)
+        } else {
+            places = places.sorted(byKeyPath: "name", ascending: ascendingSorting)
+        }
+        
+        tableView.reloadData()
+    }
+    
+    
 
+}
+
+
+extension MainViewController: UISearchResultsUpdating {
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        filterContentForSearchText(searchController.searchBar.text!)
+    }
+    
+    private func filterContentForSearchText(_ searchText: String) {
+        //будем производить поиск по поялм name и location, фильтровать данные по значению из параметра searchText вне зависимости от регистра
+        filteredPlaces = places.filter("name CONTAINS[c] %@ OR location CONTAINS[c] %@", searchText, searchText)
+        
+        tableView.reloadData()
+    }
+    
+    
 }
